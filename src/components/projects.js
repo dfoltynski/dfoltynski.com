b6ab60ca4438323995da8ff53ba189572192cba5
DziadDawid <53613664+DziadDawid@users.noreply.github.com>
2020-10-15 20:29:06 +0200

    :rocket: projects slider

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,33 +1,84 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGlobeEurope } from "@fortawesome/free-solid-svg-icons"
+import {
+  faGlobeEurope,
+  faAngleRight,
+  faAngleLeft,
+} from "@fortawesome/free-solid-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import BackgroundSvg1 from "../images/projects_background_1.svg"
 import BackgroundSvg2 from "../images/projects_background_2.svg"
+import { projects } from "../data/projects"
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel"
+import "pure-react-carousel/dist/react-carousel.es.css"
 
 const Projects = () => {
   return (
-    <section id="projects">
-      <h1 className="projects__title">meetme</h1>
-      <img
-        className="projects__img"
-        src="https://i.imgur.com/ecUxXbB.png"
-        alt="project_photo"
-      />
-      <div style={{ display: "flex" }}>
-        <FontAwesomeIcon
-          style={{ margin: "1em", width: "40px", height: "40px" }}
-          icon={faGlobeEurope}
-        />
-        <FontAwesomeIcon
-          style={{ margin: "1em", width: "40px", height: "40px" }}
-          icon={faGithub}
-        />
-      </div>
-      <p className="project__desc">
-        Application for setting up meetings with random people when you are
-        bored
-      </p>
+    <section style={{ margin: "0" }} id="projects">
+      <CarouselProvider
+        naturalSlideWidth={10}
+        naturalSlideHeight={10}
+        totalSlides={5}
+        isPlaying={true}
+        infinite={true}
+      >
+        <Slider>
+          {projects.map(project => (
+            <Slide index={project.id}>
+              <h1 className="projects__title">{project.project_title}</h1>
+              <img
+                className="projects__img"
+                src={project.project_img}
+                alt={project.project_title}
+              />
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <a href={project.website_link} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon
+                    style={{ margin: "1em", width: "40px", height: "40px" }}
+                    icon={faGlobeEurope}
+                  />
+                </a>
+                <a href={project.github_link} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon
+                    style={{ margin: "1em", width: "40px", height: "40px" }}
+                    icon={faGithub}
+                  />
+                </a>
+              </div>
+              <p className="project__desc">{project.short_desc}</p>
+            </Slide>
+          ))}
+        </Slider>
+        <div>
+          <ButtonBack
+            style={{
+              backgroundColor: "rgba(0,0,0,0)",
+              border: "none",
+              color: "#1e1e1e",
+              fontSize: "45px",
+            }}
+          >
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </ButtonBack>
+          <ButtonNext
+            style={{
+              backgroundColor: "rgba(0,0,0,0)",
+              border: "none",
+              color: "#1e1e1e",
+              fontSize: "45px",
+            }}
+          >
+            <FontAwesomeIcon icon={faAngleRight} />
+          </ButtonNext>
+        </div>
+      </CarouselProvider>
+
       <img
         className="background__svg1 background__svg1--project"
         src={BackgroundSvg1}
---
+++ src/data/projects.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    id: 0,
+    project_title: "meetme",
+    project_img: "https://i.imgur.com/ecUxXbB.png",
+    website_link: "https://meetme-app.herokuapp.com/",
+    github_link: "https://github.com/dfoltynski/meetme",
+    short_desc:
+      "Application for setting up meetings with random people when are bored",
+  },
+  {
+    id: 1,
+    project_title: "wydatek",
+    project_img: "https://i.imgur.com/ccks1OG.png",
+    website_link: "https://wydatekapp.herokuapp.com/",
+    github_link: "https://github.com/dfoltynski/wydatek",
+    short_desc:
+      "It is made for small businesses to keep track of their expenses",
+  },
+  {
+    id: 2,
+    project_title: "priva",
+    project_img: "https://i.imgur.com/BDtguqA.png",
+    website_link: "https://privaapp.herokuapp.com/",
+    github_link: "https://github.com/dfoltynski/priva",
+    short_desc: "Priva is secure, simple and private chat",
+  },
+  {
+    id: 3,
+    project_title: "koronawiruspolska",
+    project_img: "https://i.imgur.com/doAzztI.png",
+    website_link: "https://koronawiruspl.herokuapp.com/",
+    github_link: "https://github.com/dfoltynski/koronawiruspolska",
+    short_desc:
+      "It shows how many dead/confirmed/recovered people are in Poland",
+  },
+  {
+    id: 4,
+    project_title: "image-uploader",
+    project_img: "https://i.imgur.com/nzrGDDY.png",
+    website_link: "https://dfimage-uploader.herokuapp.com/",
+    github_link: "https://github.com/dfoltynski/image-uploader",
+    short_desc: "All images are stored in MongoDB and display right away",
+  },
+]
