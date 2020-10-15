3524c9dd950e4d812c7b7b996b9894ed119b61e0
DziadDawid <53613664+DziadDawid@users.noreply.github.com>
2020-10-15 03:10:05 +0200

    :rocket: home page RWD

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,21 +3,11 @@ import Header from "./header"
 import wave from "../images/wave.gif"
 import BackgroundSvg1 from "../images/homepage_background_1.svg"
 import BackgroundSvg2 from "../images/homepage_background_2.svg"
+import SEO from "../components/seo"
 
 const HomePage = () => {
   return (
     <main>
-      <Header />
-      <div className="homepage">
-        <p className="homepage__introduction">
-          Hi <img src={wave} alt="wave.gif" width="78px" /> I'm
-        </p>
-        <h1 className="homepage__name">David</h1>
-        <p className="homepage__profession">
-          <p style={{ color: "#0984E3" }}>Web Developer</p>
-          <p style={{ color: "#74B9FF", marginTop: "-24px" }}>Designer</p>
-        </p>
-      </div>
       <img
         className="background__svg1"
         src={BackgroundSvg1}
@@ -28,6 +18,17 @@ const HomePage = () => {
         src={BackgroundSvg2}
         alt="BackgroundSvg2"
       ></img>
+      <Header />
+      <div className="homepage">
+        <p className="homepage__introduction">
+          Hi <img src={wave} alt="wave.gif" className="homepage__wave" /> I'm
+        </p>
+        <h1 className="homepage__name">David</h1>
+        <p className="homepage__profession">
+          <p style={{ color: "#0984E3" }}>Web Developer</p>
+          <p style={{ color: "#74B9FF", marginTop: "-24px" }}>Designer</p>
+        </p>
+      </div>
     </main>
   )
 }
