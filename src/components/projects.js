2b5584f858211747336abc049462e31f77e6f1c0
DziadDawid <53613664+DziadDawid@users.noreply.github.com>
2020-10-15 15:24:25 +0200

    projects page

---
+++ src/components/projects.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGlobeEurope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import BackgroundSvg1 from "../images/projects_background_1.svg"
+import BackgroundSvg2 from "../images/projects_background_2.svg"
+
+const Projects = () => {
+  return (
+    <main>
+      <h1 className="projects__title">meetme</h1>
+      <img
+        className="projects__img"
+        src="https://i.imgur.com/ecUxXbB.png"
+        alt="project_photo"
+      />
+      <div style={{ display: "flex" }}>
+        <FontAwesomeIcon
+          style={{ margin: "1em", width: "56px", height: "56px" }}
+          icon={faGlobeEurope}
+        />
+        <FontAwesomeIcon
+          style={{ margin: "1em", width: "56px", height: "56px" }}
+          icon={faGithub}
+        />
+      </div>
+      <p>
+        Application for setting up meetings with random people when you are
+        bored
+      </p>
+      <img
+        className="background__svg1 background__svg1--project"
+        src={BackgroundSvg1}
+        alt="projects_background_1"
+      />
+      <img
+        className="background__svg2 background__svg2--project"
+        src={BackgroundSvg2}
+        alt="projects_background_2"
+      />
+    </main>
+  )
+}
+
+export default Projects
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HomePage from "../components/homepage"
+import Projects from "../components/projects"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomePage />
+    <Projects />
   </Layout>
 )
 
